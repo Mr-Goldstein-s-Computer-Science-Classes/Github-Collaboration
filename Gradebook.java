1e1e7afbceaacaff271cd2c2a74ff4c061ec6597
Luksh Mahajan <113115939+LukshMahajan@users.noreply.github.com>
2024-02-23 11:51:11 -0500

    Update Gradebook.java

--- Gradebook.java
+++ Gradebook.java
@@ -1,2 +1,43 @@
 public class Gradebook {
+
+    Gradebook(){
+
+    }
+    void createStudent(String name){
+
+    }
+    void addAssignment(String assignment){
+
+    }
+    void gradeAssignment(String assignment, String studentName, double grade){
+
+    }
+    double getAverageGrade(String assignment){
+
+    }
+    double getMinimumGrade(String assignment){
+
+    }
+    double getMaximumGrade(String assignment){
+
+    }
+    double getAverageOverall(){
+
+    }
+    double getMaxOverall(){
+
+    }
+    double getMinOverall(){
+
+    }
+    double getStudentsGrade(){
+
+    }
+    String toString(){
+
+    } //Returns the names of the students and the names of the assignments as a String
+    Student getStudent(String studentName){
+
+    }
+
 }
