dac98aa6bddf7d5a8d270360da149106643d2a67
Luksh Mahajan <113115939+LukshMahajan@users.noreply.github.com>
2024-02-23 12:25:40 -0500

    Update Gradebook.java

--- Gradebook.java
+++ Gradebook.java
@@ -1,25 +1,45 @@
+
+import java.util.ArrayList;
 public class Gradebook {
 
-    Gradebook(){
+    private ArrayList<Student> studentList = new ArrayList<>();
+    private ArrayList<String> assignments = new ArrayList<>();
 
+    public Gradebook(){
     }
-    void createStudent(String name){
-
+    public void createStudent(String name){
+        studentList.add(new Student(name));
     }
     void addAssignment(String assignment){
-
+        assignments.add(assignment);
     }
     void gradeAssignment(String assignment, String studentName, double grade){
-
+        studentName.gradeAssignment(assignment, grade);
     }
     double getAverageGrade(String assignment){
-
+        double totalGrade = 0.0;
+        for (Student student : studentList){
+            totalGrade += student.getAssignmentGrade(assignment);
+        }
+        return totalGrade/studentList.size();
     }
     double getMinimumGrade(String assignment){
-
+        double lowestGrade = studentList.get(0).getAssignmentGrade(assignment);
+        for (int i = 1; i < studentList.size(); i++){
+            if (studentList.get(i).getAssignmentGrade(assignment)<lowestGrade){
+                lowestGrade = studentList.get(i).getAssignmentGrade(assignment);
+            }
+        }
+        return lowestGrade;
     }
     double getMaximumGrade(String assignment){
-
+        double maxGrade = studentList.get(0).getAssignmentGrade(assignment);
+        for (int i = 1; i < studentList.size(); i++){
+            if (studentList.get(i).getAssignmentGrade(assignment)>maxGrade){
+                maxGrade = studentList.get(i).getAssignmentGrade(assignment);
+            }
+        }
+        return maxGrade;
     }
     double getAverageOverall(){
 
@@ -31,9 +51,11 @@ public class Gradebook {
 
     }
     double getStudentsGrade(){
-
+        for (String assignment : assignments){
+            
+        }
     }
-    String toString(){
+    public String toString(){
 
     } //Returns the names of the students and the names of the assignments as a String
     Student getStudent(String studentName){
